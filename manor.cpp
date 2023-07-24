3ba0957be2e9c70124effe4df66f6d666d2a5980
Conor Dockry <conordockry@gmail.com>
2023-07-24 17:56:41 -0400

    messing around, learning connect slots/signals, tab widget

--- manor.cpp
+++ manor.cpp
@@ -1,11 +1,14 @@
 #include "manor.h"
 #include "./ui_manor.h"
+#include <QListWidget>
 
-Manor::Manor(QWidget *parent)
+Manor::Manor(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::Manor)
 {
     ui->setupUi(this);
+    connect(ui->action_Quit, &QAction::triggered, this, &Manor::quitApp);
+    connect(ui->listWidget, &QListWidget::currentItemChanged, this, &Manor::updateLabel);
 }
 
 Manor::~Manor()
@@ -13,3 +16,12 @@ Manor::~Manor()
     delete ui;
 }
 
+void Manor::quitApp()
+{
+    this->close();
+}
+
+void Manor::updateLabel(QListWidgetItem* current, QListWidgetItem* previous)
+{
+    ui->labelT1P1->setText(current->text());
+}
--- manor.h
+++ manor.h
@@ -1,21 +1,29 @@
 #ifndef MANOR_H
 #define MANOR_H
 
+#include <QListWidgetItem>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Manor; }
+namespace Ui {
+class Manor;
+}
 QT_END_NAMESPACE
 
-class Manor : public QMainWindow
-{
+class Manor : public QMainWindow {
     Q_OBJECT
 
 public:
-    Manor(QWidget *parent = nullptr);
+    Manor(QWidget* parent = nullptr);
     ~Manor();
 
+private slots:
+    void quitApp();
+
+    // void on_listWidget_currentItemChanged(QListWidgetItem* current, QListWidgetItem* previous);
+    void updateLabel(QListWidgetItem* current, QListWidgetItem* previous);
+
 private:
-    Ui::Manor *ui;
+    Ui::Manor* ui;
 };
 #endif // MANOR_H
